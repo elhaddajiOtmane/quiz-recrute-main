a4a734fc6ce3976fc16f6a351bc1ec3263cfc999
ELHADADJI Otmane <39245156+elhaddajiOtmane@users.noreply.github.com>
2023-08-21 14:11:59 +0100

    add Candidates list

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -104,7 +104,9 @@ $setting = App\Setting::first();
           <!-- Optionally, you can add icons to the links -->
           <li class="{{$dash}}"><a href="{{url('/admin')}}" title="Dashboard"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
           <li class="{{$users}}"><a href="{{url('/admin/users')}}" title="Students"><i class="fa fa-users"></i> <span>Students</span></a></li>
-          <li class="{{$quiz}}"><a href="{{url('admin/topics')}}" title="Quiz"><i class="fa fa-gears"></i> <span>Quiz</span></a></li>          
+          <li class="{{ isset($cand) ? $cand : '' }}"><a href="{{ url('/admin/candidates') }}" title="Candidates"><i class="fa fa-users"></i> <span>Candidates</span></a></li>
+                
+           <li class="{{$quiz}}"><a href="{{url('admin/topics')}}" title="Quiz"><i class="fa fa-gears"></i> <span>Quiz</span></a></li>          
           <li class="{{$questions}}"><a href="{{url('admin/questions')}}" title="Questions"><i class="fa fa-question-circle-o"></i> <span>Questions</span></a></li>          
           <li class="{{$all_re}}"><a href="{{url('/admin/all_reports')}}" title="Student Report"><i class="fa fa-file-text-o"></i> <span>Student Report</span></a></li>
           <li class="{{$top_re}}"><a href="{{url('/admin/top_report')}}" title="Top Student Report"><i class="fa fa-user"></i> <span>Top Student Report</span></a></li>
