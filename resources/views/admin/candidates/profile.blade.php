f53ed0075675bf5aea6b87ccfe8d54307ec2e577
ELHADADJI Otmane <39245156+elhaddajiOtmane@users.noreply.github.com>
2023-08-21 16:07:58 +0100

    +91 to +212

--- resources/views/admin/candidates/profile.blade.php
+++ resources/views/admin/candidates/profile.blade.php
@@ -59,7 +59,7 @@
               <div class="col-md-6 margin-bottom">
                 <div class="form-group{{ $errors->has('mobile') ? ' has-error' : '' }}">
                   {!! Form::label('mobile', 'Mobile No.') !!}
-                  {!! Form::text('mobile', null, ['class' => 'form-control', 'placeholder' => 'eg: +212-123-456-7890']) !!}
+                  {!! Form::text('mobile', null, ['class' => 'form-control', 'placeholder' => 'eg: +91-123-456-7890']) !!}
                   <small class="text-danger">{{ $errors->first('mobile') }}</small>
                 </div>
                 <div class="form-group{{ $errors->has('city') ? ' has-error' : '' }}">
@@ -108,7 +108,7 @@
 
                 <div class="form-group{{ $errors->has('mobile') ? ' has-error' : '' }}">
                   {!! Form::label('mobile', 'Mobile No.') !!}
-                  {!! Form::text('mobile', null, ['class' => 'form-control', 'placeholder' => 'eg: +212-123-456-7890']) !!}
+                  {!! Form::text('mobile', null, ['class' => 'form-control', 'placeholder' => 'eg: +91-123-456-7890']) !!}
                   <small class="text-danger">{{ $errors->first('mobile') }}</small>
                 </div>
               </div>
